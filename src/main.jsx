9f2430364ec196e78c2d9136ee83dcb495cb1fd1
Biagio Cota <biagiocota24@gmail.com>
2026-05-14 16:54:25 +0200

    adds redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import store from "./redux/store/index.js";
 import { Provider } from "react-redux";
+import { store, persistedStore } from "./redux/store/index.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistedStore}>
+      <App />
+    </PersistGate>
   </Provider>,
 );
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,6 +2,13 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import mainReducer from "../reducers";
 import detailsReducer from "../reducers/detailsReducer";
 import favoriteReducer from "../reducers/favoriteReducer";
+import { persistReducer, persistStore } from "redux-persist";
+import localStorage from "redux-persist/es/storage";
+
+const persistConfig = {
+  storage: localStorage,
+  key: "root",
+};
 
 const bigReducer = combineReducers({
   main: mainReducer,
@@ -9,8 +16,12 @@ const bigReducer = combineReducers({
   favorite: favoriteReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, bigReducer);
+
 const store = configureStore({
-  reducer: bigReducer,
+  reducer: persistedReducer,
 });
 
-export default store;
+const persistedStore = persistStore(store);
+
+export { store, persistedStore };
