e3c1750168bb7aa4a9bdf67789d2a88011ec0773
Biagio Cota <biagiocota24@gmail.com>
2026-05-14 14:06:54 +0200

    Update JobDetails.jsx

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Container, Row, Col } from "react-bootstrap";
-import Job from "./Job";
+import { Container, Row, Col, Card, ListGroup, Button } from "react-bootstrap";
+// import Job from "./Job";
 import { setDetailJob } from "../redux/reducers";
 import { useEffect } from "react";
 const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs";
@@ -10,6 +10,8 @@ const JobDetails = function () {
   const job = useSelector((state) => state.main.job);
   const dispatch = useDispatch();
   const params = useParams();
+
+  const 
 
   const callData = function () {
     fetch(baseEndpoint)
@@ -28,10 +30,22 @@ const JobDetails = function () {
     <Container>
       <Row>
         <Col xs={10} className="mx-auto my-3">
-          <h1 className="display-1">Details for this Job</h1>
+          <h1 className="display-1">
+            Details for
+            {job ? (
+              <span className="text-warning fw-bold">{job.title}</span>
+            ) : (
+              <p>Caricamento...</p>
+            )}
+          </h1>
         </Col>
         <Col xs={10} className="mx-auto mb-5">
-          {job ? <Job data={job} /> : <p>Caricamento...</p>}
+          {job ? (
+            <div dangerouslySetInnerHTML={{ __html: job.description }} />
+          ) : (
+            <p>Caricamento...</p>
+          )}
+          <Button>Scarta annuncio</Button>
         </Col>
       </Row>
     </Container>
